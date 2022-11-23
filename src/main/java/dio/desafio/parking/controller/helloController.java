42c916d9a36e61de2919df4dd5772825b035fd35
Lucas <bezerra_lucas5@hotmail.com>
2022-11-22 21:38:35 -0300

    testando o rest controller 2

--- src/main/java/dio/desafio/parking/controller/helloController.java
+++ src/main/java/dio/desafio/parking/controller/helloController.java
@@ -4,8 +4,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping
 @RestController
+@RequestMapping("/")
 public class helloController {
     @GetMapping
     public String Hello(){return "Ai jojós";}
