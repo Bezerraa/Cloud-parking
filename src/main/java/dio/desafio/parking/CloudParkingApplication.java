93c9f7956a3f3b52e7fa561c8fb743b013cf0f7d
Lucas <bezerra_lucas5@hotmail.com>
2022-11-22 22:20:18 -0300

    testando o rest controller 5

--- src/main/java/dio/desafio/parking/CloudParkingApplication.java
+++ src/main/java/dio/desafio/parking/CloudParkingApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CloudParkingApplication {
-
+//reassistir a 6ª aula. [...] criação de endpoint de cadastro.
 	public static void main(String[] args) {
 		SpringApplication.run(CloudParkingApplication.class, args);
 		String name = "Lucas";
