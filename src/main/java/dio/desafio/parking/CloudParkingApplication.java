4eb943692c062aa33ba36f5309de889f3c0749fb
Lucas <bezerra_lucas5@hotmail.com>
2022-11-22 21:22:52 -0300

    adicionando sys properties

--- src/main/java/dio/desafio/parking/CloudParkingApplication.java
+++ src/main/java/dio/desafio/parking/CloudParkingApplication.java
@@ -9,7 +9,7 @@ public class CloudParkingApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CloudParkingApplication.class, args);
 		String name = "Lucas";
-		
+
 	}
 
 }
