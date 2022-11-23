a1832544c6a815f86d0a482b9ac453180b43feb4
Lucas <bezerra_lucas5@hotmail.com>
2022-11-22 21:19:48 -0300

    primeira alteração

--- src/main/java/dio/desafio/parking/CloudParkingApplication.java
+++ src/main/java/dio/desafio/parking/CloudParkingApplication.java
@@ -8,6 +8,8 @@ public class CloudParkingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudParkingApplication.class, args);
+		String name = "Lucas";
+		
 	}
 
 }
