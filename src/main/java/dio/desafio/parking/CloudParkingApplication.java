a4fb8ce977f487cbdfd8009a3a0c94e7404a6812
Lucas <bezerra_lucas5@hotmail.com>
2022-11-24 11:00:42 -0300

    adding Testing methods

--- src/main/java/dio/desafio/parking/CloudParkingApplication.java
+++ src/main/java/dio/desafio/parking/CloudParkingApplication.java
@@ -4,7 +4,7 @@ import dio.desafio.parking.config.SwaggerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+//Realizando os testes da API e configurando as portas da aplicação
 @SpringBootApplication
 public class CloudParkingApplication {
 
---
+++ src/test/java/dio/desafio/parking/controller/ParkingControllerIT.java
@@ -0,0 +1,52 @@
+package dio.desafio.parking.controller;
+
+import dio.desafio.parking.controller.dto.ParkingCreateDTO;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import static org.junit.jupiter.api.Assertions.*;
+//Porta aleatória para que se houverem outros testes feitos por outros desenvolvedores,
+//que não choquem com as portas.
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class ParkingControllerIT {
+    @LocalServerPort
+    private int randomport;
+
+    @BeforeEach //anotação do Junit -> Para cada teste realizado, esse método será executado
+    public void setUpTest(){
+        RestAssured.port = randomport;
+    }
+
+    @Test
+    void whenfindAllThenCheckResult() {
+        RestAssured.given()
+                .when()
+                .get("/parking")
+                .then()
+                .statusCode(HttpStatus.OK.value());
+
+    }
+
+    @Test
+    void whenCreateThenCheckIsCreated() {
+        var createDTO = new ParkingCreateDTO();
+        createDTO.setColor("AZUL");
+        createDTO.setModel("FORD FIESTA");
+        createDTO.setLicense("QWW-3123");
+        createDTO.setState("MA");
+
+
+        RestAssured.given()
+                .when()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(createDTO)
+                .post()
+                .then()
+                .statusCode(HttpStatus.CREATED.value());
+    }
+}
